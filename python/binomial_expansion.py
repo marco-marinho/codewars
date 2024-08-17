8b01e4a6cbff1cfd07c450d3d6215b54bb932776
Marco Marinho <marcoobom@gmail.com>
2024-08-17 20:15:28 +0200

    Binomial Expansion

---
+++ python/binomial_expansion.py
@@ -0,0 +1,36 @@
+from math import factorial
+import re
+
+
+def binomial_coefficient(n, k):
+    return int(factorial(n) / (factorial(k) * factorial(n - k)))
+
+
+def parse_match(istring):
+    if istring == "-":
+        return -1
+    if istring == "":
+        return 1
+    return int(istring)
+
+
+def parse(istring):
+    expr = re.compile(r"\((-?[0-9]*)?(\D)([+|-][0-9]*)\)\^([0-9]*)")
+    match = expr.match(istring)
+    return parse_match(match.group(1)), match.group(2), int(match.group(3)), int(match.group(4))
+
+
+def expand(expr):
+    a, x, b, n = parse(expr)
+    output = ""
+    for i in range(n + 1):
+        term = binomial_coefficient(n, i) * a ** (n - i) * b ** i
+        if n - i == 0 or abs(term) != 1:
+            output += f"{term:+d}"
+        else:
+            output += "+" if term == 1 else "-"
+        if n - i > 1:
+            output += f"{x}^{n - i}"
+        elif n - i == 1:
+            output += f"{x}"
+    return output if output[0] != "+" else output[1:]
--- python/rail_fence_cypher.py
+++ python/rail_fence_cypher.py
@@ -2,16 +2,15 @@ from itertools import zip_longest, chain
 
 
 def helper(src, n):
-    a, b = (n - 2) * 2 + 2, 0
+    ar, br = range((n - 2) * 2 + 2, -1, -2), range(0, (n - 2) * 2 + 3, 2)
     output = []
     for i in range(min(n, len(src))):
         output.append(src[i])
+        a, b = ar[i], br[i]
         first = src[i + a::a + b] if a > 0 else ""
         second = src[i + a + b::b + a] if b > 0 else ""
-        output.extend(chain.from_iterable(zip_longest(first, second, fillvalue="")))
-        a -= 2
-        b += 2
-    return [i for i in output if i != ""]
+        output.extend(chain.from_iterable(zip_longest(first, second, fillvalue=None)))
+    return [i for i in output if i is not None]
 
 
 def encode_rail_fence_cipher(string, n):
@@ -19,8 +18,7 @@ def encode_rail_fence_cipher(string, n):
 
 
 def decode_rail_fence_cipher(string, n):
-    idxs = helper(range(len(string)), n)
     output = [""] * len(string)
-    for i, element in zip(idxs, string):
+    for i, element in zip(helper(range(len(string)), n), string):
         output[i] = element
     return "".join(output)
