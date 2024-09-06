bce298c55b8da5e20dd8a8e011433f3bdd7fbd0f
Marco Marinho <marcoobom@gmail.com>
2024-09-06 21:17:06 +0200

    first non repeating character

---
+++ python/first_non_repeating_character.py
@@ -0,0 +1,10 @@
+from collections import Counter
+
+def first_non_repeating_letter(s):
+    if not s:
+        return ""
+    count = Counter(s.lower())
+    for c in s:
+        if count[c.lower()] == 1:
+            return c
+    return ""
