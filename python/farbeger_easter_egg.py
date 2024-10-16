5b320664ad95f8e3ab2480064ef4b615e3a7f9fc
Marco Marinho <marcoobom@gmail.com>
2024-10-16 19:28:10 +0200

    farbeger easter egg

---
+++ python/farbeger_easter_egg.py
@@ -0,0 +1,13 @@
+def height(n, m):  
+    if n == 0 or m==0:
+        return 0
+    if n > m:
+        return height(m, m)
+    c = 1
+    b = 1
+    a = 0
+    while c <= n:
+        b = (m + 1 - c) * b // c
+        c += 1
+        a += b
+    return a
