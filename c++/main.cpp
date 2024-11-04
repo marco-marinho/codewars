fc528ac89356e9442c4b4e2839943fb9438405be
Marco Marinho <marcoobom@gmail.com>
2024-11-04 20:19:52 +0100

    Factorial of a big number

--- c++/main.cpp
+++ c++/main.cpp
@@ -1,9 +1,45 @@
-#include <iostream>
 #include <string>
-#include "multiply_strings.cpp"
+#include <vector>
+#include <sstream>
+#include <deque>
+#include <iostream>
 
+std::string factorial(int factorial)
+{
+    if (factorial < 0)
+    {
+        return "";
+    }
+    if (factorial == 0)
+    {
+        return "1";
+    }
+    std::deque<int> elements{1};
+    for (int i = 2; i <= factorial; i++)
+    {
+            int carry = 0;
+            for (size_t j = 0; j < elements.size(); j++)
+            {
+                int current = elements[j] * i + carry;
+                elements[j] = current % 10;
+                carry = current / 10;
+            }
+            while (carry)
+            {
+                elements.push_back(carry % 10);
+                carry /= 10;
+            }
+    }
+    std::stringstream ss;
+    for (auto it = elements.rbegin(); it != elements.rend(); it++)
+    {
+        ss << *it;
+    }
+    return ss.str();
+}
 
-int main(){
-    std::cout << multiply("1020303004875647366210", "2774537626200857473632627613") << std::endl;
+int main()
+{
+    std::cout << factorial(25) << std::endl;
     return 0;
 }
